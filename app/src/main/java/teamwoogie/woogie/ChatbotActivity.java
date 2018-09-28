50a721cf887e438112bd104aecd5969aa81d1935
Miji Park <ppmj789@naver.com>
2018-09-28 18:18:14 +0900

    기본틀생성, 지정된 시간에 행동수행(알람), 아이콘 및 UI 생성

---
+++ app/src/main/java/teamwoogie/woogie/ChatbotActivity.java
@@ -0,0 +1,15 @@
+package teamwoogie.woogie;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class ChatbotActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot);
+    }
+}
