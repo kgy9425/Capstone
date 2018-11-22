7264e84feaff9802ddaad49322a6838dc6f18181
SoohyunPark <tntnrr@naver.com>
2018-11-22 19:52:16 +0900

    질병코드 DB 입력

--- woogie-gayoung/app/src/main/java/teamwoogie/woogie/HealthActivity.java
+++ woogie-gayoung/app/src/main/java/teamwoogie/woogie/HealthActivity.java
@@ -1,51 +1,75 @@
 package teamwoogie.woogie;
 
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.content.SharedPreferences;
-        import android.content.res.AssetManager;
-        import android.graphics.Bitmap;
-        import android.graphics.BitmapFactory;
-        import android.graphics.drawable.BitmapDrawable;
-        import android.graphics.drawable.Drawable;
-        import android.net.Uri;
-        import android.os.Bundle;
-        import android.os.Environment;
-        import android.provider.MediaStore;
-        import android.support.v7.app.AlertDialog;
-        import android.support.v7.app.AppCompatActivity;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.googlecode.tesseract.android.TessBaseAPI;
-        import java.io.BufferedInputStream;
-        import java.io.BufferedOutputStream;
-        import java.io.File;
-        import java.io.FileNotFoundException;
-        import java.io.FileOutputStream;
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.OutputStream;
-
+import com.googlecode.tesseract.android.TessBaseAPI;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HealthActivity extends AppCompatActivity {
-    private static final int PICK_FROM_CAMERA = 0;
-    private static final int PICK_FROM_ALBUM = 1;
-    private static final int CROP_FROM_iMAGE = 2;
 
-    private Uri mImageCaptureUri;
-    private ImageView iv_UserPhoto;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    // private Uri mImageCaptureUri;
+    // private ImageView iv_UserPhoto;
     private Button b;
     private int id_view;
-    private String absoultePath;
+    // private String absoultePath;
 
-    Bitmap recordPicture = null;
+    ////Bitmap recordPicture = null;
     private TessBaseAPI mTess; //Tess API reference
     String datapath = "" ; //언어데이터가 있는 경로
+
+
+    /////// new SooHyun ////
+    private String imageFilePath;
+    private Uri photoUri;
+    private String timeStamp;
+    private String OCRresult;
+
+    Bitmap recordPicture = null;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,162 +77,133 @@ public class HealthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
 
-        iv_UserPhoto = (ImageView) this.findViewById(R.id.imageView);
+        // iv_UserPhoto = (ImageView) this.findViewById(R.id.imageView);
         b = (Button) this.findViewById(R.id.add);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                doTakeAlbumAction();
+                //doTakeAlbumAction();
+                sendTakePhotoIntent();
             }
         });
 
 
-        datapath = getFilesDir()+ "/tesseract/";
+        datapath = getFilesDir() + "/tesseract/";
         checkFile(new File(datapath + "tessdata/"));
 
         //Tesseract API
-        String lang = "eng";
+        String lang_eng = "eng";
+        //String lang_kor = "kor";
 
         mTess = new TessBaseAPI();
-        mTess.init(datapath, lang);
+        mTess.init(datapath, lang_eng);
+        //  mTess.init(datapath, lang_kor);
 
 
     }
 
-    public void doTakePhotoAction() // 카메라 촬영 후 이미지 가져오기
-    {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        // 임시로 사용할 파일의 경로를 생성
-        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
-        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+    //카메라로 사진찍는 메소드
+    private void sendTakePhotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
 
-        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
-        startActivityForResult(intent, PICK_FROM_CAMERA);
+            if (photoFile != null) {
+                photoUri = FileProvider.getUriForFile(this, getPackageName(), photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+        // putExtra()를 이용해서 이미지가 저장될 uri를 같이 넘김
+
+
+    }
+///
+    ///// new SooHyun /////
+
+    //// 이미지가 저장 될 파일을 만드는 함수////
+    private File createImageFile() throws IOException {
+        timeStamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String imageFIleName = "TEST_" + timeStamp + "_";
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFIleName,
+                ".jpg",
+                storageDir
+        );
+
+        imageFilePath = image.getAbsolutePath();
+        return image;
     }
 
-    public void doTakeAlbumAction() // 앨범에서 이미지 가져오기
-    {
-        // 앨범 호출
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-        startActivityForResult(intent, PICK_FROM_ALBUM);
-    }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            Bitmap imageBitmap = BitmapFactory.decodeFile(imageFilePath);
+            ExifInterface exif = null;
 
-        if (resultCode != RESULT_OK)
-            return;
-
-        switch (requestCode) {
-            case PICK_FROM_ALBUM: {
-                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
-                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
-                mImageCaptureUri = data.getData();
-                Log.d("SmartWheel", mImageCaptureUri.getPath().toString());
-                Log.e("tag", "album");
+            try {
+                exif = new ExifInterface(imageFilePath);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            case PICK_FROM_CAMERA: {
-                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
-                mImageCaptureUri = data.getData();
-                Log.d("SmartWheel", mImageCaptureUri.getPath().toString());
-                Log.e("tag", "camera");
+            int exifOrientation;
+            int exifDegree;
 
-                Intent intent = new Intent("com.android.camera.action.CROP");
-                intent.setDataAndType(mImageCaptureUri, "image/*");
-
-                // CROP할 이미지를 200*200 크기로 저장
-                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
-                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
-                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
-                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
-                intent.putExtra("scale", true);
-                intent.putExtra("return-data", true);
-                startActivityForResult(intent, CROP_FROM_iMAGE); // CROP_FROM_CAMERA case문 이동
-                break;
+            if (exif != null) {
+                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                exifDegree = exifOrientationToDegrees(exifOrientation);
+            } else {
+                exifDegree = 0;
             }
-            case CROP_FROM_iMAGE: {
-                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
-                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-                // 임시 파일을 삭제합니다.
-                if (resultCode != RESULT_OK) {
-                    return;
-                }
 
-                final Bundle extras = data.getExtras();
-
-                // CROP된 이미지를 저장하기 위한 FILE 경로
-                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
-                        "/SmartWheel/" + System.currentTimeMillis() + ".jpg";
-
-                if (extras != null) {
-                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
-                    iv_UserPhoto.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
-                    ///ocr
-                    BitmapDrawable d = (BitmapDrawable) iv_UserPhoto.getDrawable();
-                    recordPicture = d.getBitmap();
-
-                    /////
-
-                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
-                    absoultePath = filePath;
-                    break;
-
-                }
-                // 임시 파일 삭제
-                /*
-                File f = new File(mImageCaptureUri.getPath());
-                if (f.exists()) {
-                    f.delete();
-                }
-                */
-            }
-        }
-
-
-    }
-
-    /*
-     * Bitmap을 저장하는 부분
-     */
-    private void storeCropImage(Bitmap bitmap, String filePath) {
-        // SmartWheel 폴더를 생성하여 이미지를 저장하는 방식이다.
-        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SmartWheel";
-        File directory_SmartWheel = new File(dirPath);
-
-        if (!directory_SmartWheel.exists()) // SmartWheel 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
-            directory_SmartWheel.mkdir();
-
-        File copyFile = new File(filePath);
-        BufferedOutputStream out = null;
-
-        try {
-
-            copyFile.createNewFile();
-            out = new BufferedOutputStream(new FileOutputStream(copyFile));
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-
-            // sendBroadcast를 통해 Crop된 사진을 앨범에 보이도록 갱신한다.
-            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-                    Uri.fromFile(copyFile)));
-
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+            ((ImageView)findViewById(R.id.imageView)).setImageBitmap(rotate(imageBitmap, exifDegree));
         }
     }
 
-    //Process an Image
+    private int exifOrientationToDegrees(int exifOrientation)
+    {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        recordPicture = null;
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        recordPicture = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+        return recordPicture;
+    }
+
+    ///////////////////////////////////////////////////////
     public void processImage(View view) {
-        String OCRresult = null;
-        mTess.setImage(recordPicture);
+
+        mTess.setImage(recordPicture); //여기 고쳐야함
         OCRresult = mTess.getUTF8Text();
+
+
         TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
         OCRTextView.setText(OCRresult);
+
+        insertToDatabase("SH", OCRresult, timeStamp);
+
     }
 
     private void copyFiles() {
@@ -248,5 +243,76 @@ public class HealthActivity extends AppCompatActivity {
             }
         }
     }
+
+
+
+
+    private void insertToDatabase(String UserID, String DiseaseCode,String Date) {
+        class InsertData extends AsyncTask<String, String, String> {
+            ProgressDialog loading;
+
+            protected void onPreExecute() {
+                super.onPreExecute();
+                ///
+                loading = ProgressDialog.show(HealthActivity.this, "Please Wait", null, true, true);
+            }
+
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    String UserID = (String) params[0];
+                    String DiseaseCode = (String) params[1];
+                    String Date = (String) params[2];
+
+                    String link = "http://ppmj789.dothome.co.kr/php/insertDiseaseRecord.php?";
+                    String data = URLEncoder.encode("UserID", "UTF-8") + "=" + URLEncoder.encode(UserID, "UTF-8");
+                    data += "&" + URLEncoder.encode("DiseaseCode", "UTF-8") + "=" + URLEncoder.encode(DiseaseCode, "UTF-8");
+                    data += "&" + URLEncoder.encode("Date", "UTF-8") + "=" + URLEncoder.encode(Date, "UTF-8");
+
+                    link+=data;
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+        InsertData task = new InsertData();
+        task.execute(UserID, DiseaseCode,Date);
+
+
+    }
+
+
+
 
 }
