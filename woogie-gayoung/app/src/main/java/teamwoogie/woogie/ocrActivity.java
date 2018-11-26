7e4cd33768741f3db81597553f1d4e9eb6f02fb2
Miji Park <ppmj789@naver.com>
2018-11-26 16:31:52 +0900

    OCR 수정

--- woogie-gayoung/app/src/main/java/teamwoogie/woogie/ocrActivity.java
+++ woogie-gayoung/app/src/main/java/teamwoogie/woogie/ocrActivity.java
@@ -1,22 +1,28 @@
 package teamwoogie.woogie;
+
+
+import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
+import android.media.MediaScannerConnection;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -26,9 +32,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -41,18 +47,27 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-
-public class ocrActivity extends AppCompatActivity {
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+import java.util.List;
 
 
-    // private Uri mImageCaptureUri;
-    // private ImageView iv_UserPhoto;
-    private Button b;
-    private int id_view;
-    // private String absoultePath;
+public class ocrActivity extends AppCompatActivity{
+    private ImageView imgMain;
+    private Button btnCamera, btnAlbum;
 
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_ALBUM = 2;
+    private static final int CROP_FROM_CAMERA = 3;
+
+    private Uri photoUri;
+    private String[] permissions = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
+    private static final int MULTIPLE_PERMISSIONS = 101;
+
+    private String mCurrentPhotoPath;
     ////Bitmap recordPicture = null;
     private TessBaseAPI mTess; //Tess API reference
     String datapath = "" ; //언어데이터가 있는 경로
@@ -60,7 +75,7 @@ public class ocrActivity extends AppCompatActivity {
 
     /////// new SooHyun ////
     private String imageFilePath;
-    private Uri photoUri;
+
     private String timeStamp;
 
     private Date today;
@@ -70,22 +85,32 @@ public class ocrActivity extends AppCompatActivity {
     Bitmap recordPicture = null;
 
 
-
     @Override
-
-    public void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
-        // iv_UserPhoto = (ImageView) this.findViewById(R.id.imageView);
-        b = (Button) this.findViewById(R.id.add);
-        b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        checkPermissions();
 
-                sendTakePhotoIntent();
+        imgMain = (ImageView) findViewById(R.id.imageView);
+        btnCamera = (Button) findViewById(R.id.camera);
+        btnAlbum = (Button) findViewById(R.id.album);
+
+
+
+        btnCamera.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                takePhoto();
             }
         });
-
+        btnAlbum.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                goToAlbum();
+            }
+        });
 
         datapath = getFilesDir() + "/tesseract/";
         checkFile(new File(datapath + "tessdata/"));
@@ -102,83 +127,157 @@ public class ocrActivity extends AppCompatActivity {
     }
 
 
-    //카메라로 사진찍는 메소드
-    private void sendTakePhotoIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
 
-            if (photoFile != null) {
-                photoUri = FileProvider.getUriForFile(this, getPackageName(), photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        initView();
+
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> permissionList = new ArrayList<>();
+        for (String pm : permissions) {
+            result = ContextCompat.checkSelfPermission(this, pm);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(pm);
             }
         }
-        // putExtra()를 이용해서 이미지가 저장될 uri를 같이 넘김
-
-
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionList.toArray(new String[permissionList.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
-///
-    ///// new SooHyun /////
 
-    //// 이미지가 저장 될 파일을 만드는 함수////
+
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(ocrActivity.this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+            finish();
+            e.printStackTrace();
+        }
+        if (photoFile != null) {
+            photoUri = FileProvider.getUriForFile(ocrActivity.this,
+                    "teamwoogie.woogie.provider", photoFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(intent, PICK_FROM_CAMERA);
+        }
+    }
+
     private File createImageFile() throws IOException {
-        // timeStamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        java.util.Date currentDate = new java.util.Date();
-        java.text.SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd");
-
-        timeStamp = format.format(currentDate);
-
-
-
-
-        String imageFIleName = "TEST_" + timeStamp + "_";
-
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFIleName,
-                ".jpg",
-                storageDir
-        );
-
-        imageFilePath = image.getAbsolutePath();
+        timeStamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String imageFileName = "nostest_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/NOSTest/");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
     }
 
+    private void goToAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //Bundle extras = data.getExtras();
-            Bitmap imageBitmap = BitmapFactory.decodeFile(imageFilePath);
-            ExifInterface exif = null;
 
-            try {
-                exif = new ExifInterface(imageFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            int exifOrientation;
-            int exifDegree;
-
-            if (exif != null) {
-                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-                exifDegree = exifOrientationToDegrees(exifOrientation);
-            } else {
-                exifDegree = 0;
-            }
-
-            ((ImageView)findViewById(R.id.imageView)).setImageBitmap(rotate(imageBitmap, exifDegree));
+    // @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.camera:
+                takePhoto();
+                break;
+            case R.id.album:
+                goToAlbum();
+                break;
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++) {
+                        if (permissions[i].equals(this.permissions[0])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showNoPermissionToastAndFinish();
+                            }
+                        } else if (permissions[i].equals(this.permissions[1])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showNoPermissionToastAndFinish();
+
+                            }
+                        } else if (permissions[i].equals(this.permissions[2])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showNoPermissionToastAndFinish();
+
+                            }
+                        }
+                    }
+                } else {
+                    showNoPermissionToastAndFinish();
+                }
+                return;
+            }
+        }
+    }
+
+    private void showNoPermissionToastAndFinish() {
+        Toast.makeText(this, "권한 요청에 동의 해주셔야 이용 가능합니다. 설정에서 권한 허용 하시기 바랍니다.", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (requestCode == PICK_FROM_ALBUM) {
+            if (data == null) {
+                return;
+            }
+            photoUri = data.getData();
+            cropImage();
+        } else if (requestCode == PICK_FROM_CAMERA) {
+            cropImage();
+            // 갤러리에 나타나게
+            MediaScannerConnection.scanFile(ocrActivity.this,
+                    new String[]{photoUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+        } else if (requestCode == CROP_FROM_CAMERA) {
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+                //recordPicture = ThumbnailUtils.extractThumbnail(bitmap, 128, 128);
+                Matrix matrix = new Matrix();
+                recordPicture = null;
+
+                recordPicture = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                //recordPicture.compress(Bitmap.CompressFormat.JPEG, 100, bs); //이미지가 클 경우 압축
+
+                ((ImageView)findViewById(R.id.imageView)).setImageBitmap(recordPicture);
+
+
+            }catch (Exception e){
+                Log.e("ERROR", e.getMessage().toString());
+            }
+
+
+        }
+    }
+
+/*
     private int exifOrientationToDegrees(int exifOrientation)
     {
         if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
@@ -198,6 +297,71 @@ public class ocrActivity extends AppCompatActivity {
         recordPicture = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
         return recordPicture;
+    }
+*/
+
+    //Android N crop image
+    public void cropImage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.grantUriPermission("com.android.camera", photoUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(photoUri, "image/*");
+
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            grantUriPermission(list.get(0).activityInfo.packageName, photoUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        int size = list.size();
+        if (size == 0) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            Toast.makeText(this, "용량이 큰 사진의 경우 시간이 오래 걸릴 수 있습니다.", Toast.LENGTH_SHORT).show();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+            intent.putExtra("crop", "true");
+
+            intent.putExtra("scale", true);
+            File croppedFileName = null;
+            try {
+                croppedFileName = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            File folder = new File(Environment.getExternalStorageDirectory() + "/NOSTest/");
+            File tempFile = new File(folder.toString(), croppedFileName.getName());
+
+            photoUri = FileProvider.getUriForFile(ocrActivity.this,
+                    "teamwoogie.woogie.provider", tempFile);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+
+            intent.putExtra("return-data", false);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+
+            Intent i = new Intent(intent);
+            ResolveInfo res = list.get(0);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                i.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                grantUriPermission(res.activityInfo.packageName, photoUri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+            i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            startActivityForResult(i, CROP_FROM_CAMERA);
+        }
     }
 
     ///////////////////////////////////////////////////////
@@ -319,6 +483,10 @@ public class ocrActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
 
 
 
